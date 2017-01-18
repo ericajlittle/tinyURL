20243c5c852677fcc3a77742b5dc8a1828092f3b
ericajlittle <ericalittle123@gmail.com>
2017-01-18 03:34:48 +0000

    completed urlshortening part 2

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+var PORT = process.env.PORT || 8080;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -8,13 +11,27 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+function generateRandomString() {
+  let random = Math.random().toString(36).substring(0, 6);
+  return random;
+}
 
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  let longURL = req.body["longURL"];
+  let shortURL = generateRandomString();
+  console.log(longURL);
+  console.log(shortURL);
+  urlDatabase[shortURL] = longURL;
+  console.log(urlDatabase);
+  res.redirect(302, "http://localhost:8080/urls/"+shortURL);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -33,4 +50,17 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  if (longURL != null) {
+  res.redirect(longURL);
+  } else {
+    res.end("ERROR");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
