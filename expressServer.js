b1f167da5a5665ab93598594389ac7f3a665b534
ericajlittle <ericalittle123@gmail.com>
2017-01-31 21:51:28 +0000

    final touches completed all response messages

--- expressServer.js
+++ expressServer.js
@@ -1,30 +1,24 @@
 const express = require("express");
-// const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = process.env.PORT || 8080;
 const bcrypt = require("bcrypt");
 const cookieSession = require("cookie-session")
-
 const bodyParser = require("body-parser");
 
-
-app.use(bodyParser.urlencoded({extended: true}));
-// app.use(cookieParser())
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
   name: "session",
   secret: "mykey",
-
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+}));
 
 app.use(function(req, res, next){
 
   let user = usersDatabase[req.session.user_id];
 
   res.locals.current_user = user;
-  //find out what next is
   next();
 });
 
@@ -47,35 +41,46 @@ function emailIsTaken(email) {
 };
 
 function loggedInCheck(req, res, next){
-  if (res.locals.current_user) {
+  console.log('loggedInCheck', req.session);
+  if (req.session && req.session.user_id) {
     next();
   } else {
     // go away, or I shall taunt you a second time
     res.status(401).render("please_log_in");
   }
-}
+};
+
+function checkURLOwnership(req, res, next) {
+  const shortURL = req.params.shortURL;
+  if (!urlDatabase[shortURL]) {
+    res.status(404);
+    res.send("Tiny URL does not exist");
+
+  } else if (urlDatabase[shortURL].user_id !== req.session.user_id) {
+    res.status(403);
+    res.send("Tiny URL does not match user");
+
+  } else {
+    next();
+  }
+};
 
 app.get("/urls", loggedInCheck, (req, res) => {
   let temp = 0;
   let templateVars;
-  templateVars = { urls: urlDatabase, flag:false, user_id: res.locals.current_user.id };
+  templateVars = { urls: urlDatabase, flag:false, user_id: req.session.user_id };
   res.render("urls_index", templateVars);
 });
 
 app.post("/urls", loggedInCheck, (req, res) => {
-  // if (!res.locals.current_user) {
-  //   res.status(401).render("please_log_in");
-  //   return;
-  // }
   const longURL = req.body["longURL"];
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = {
     longURL: longURL,
     shortURL: shortURL,
-    user_id: res.locals.current_user.id
+    user_id: req.session.user_id
   }
-  // just changed from /urls
-  res.redirect(302, "/urls");
+  res.redirect(`/urls/${shortURL}`);
 });
 
 
@@ -87,10 +92,15 @@ function authenticate(email, password) {
     }
   }
   return null;
-}
+};
+
 app.get("/login", (req, res) => {
-  res.render("login");
-})
+  if (!req.session.user_id) {
+    res.render("login");
+    return;
+  }
+  res.redirect("/");
+});
 
 app.post("/login", (req, res) => {
   let email = req.body.email;
@@ -101,8 +111,8 @@ app.post("/login", (req, res) => {
 
   if (user_id) {
     req.session.user_id = user_id;
-    // res.cookie("user_id", user_id);
     res.redirect("/");
+    return;
   } else {
     res.status(401);
     res.send("wrong email or password!");
@@ -111,7 +121,12 @@ app.post("/login", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  res.render("register");
+  if (!req.session.user_id) {
+    res.render("register");
+    return;
+  }
+
+  res.redirect("/");
 });
 
 app.post("/register", (req, res) => {
@@ -133,68 +148,45 @@ app.post("/register", (req, res) => {
   const id = generateRandomString()
   usersDatabase[id] = {
     email: req.body.email,
-    //changed but might not work
     password: hashed_password,
     id: id
   }
-  // res.cookie("user_id", id);
   req.session.user_id = id;
   res.redirect("/");
-
 });
 
 app.post("/logout", (req, res) => {
   req.session.user_id = "";
-  //res.clearCookie('user_id');
-  res.redirect("/login");
+  res.redirect("/");
 });
 
 app.get("/", (req, res) => {
+  if (!req.session.user_id) {
+  res.redirect("/login");
+  return;
+  }
   res.redirect("/urls");
+  return;
 });
 
 app.get("/urls/new", loggedInCheck, (req, res) => {
-
-  // if (!res.locals.current_user) {
-  //   res.redirect(401, "/login");
-  //   return;
-  // }
   res.render("urls_new");
 });
 
-
 app.post("/urls/:shortUrl/delete", loggedInCheck, (req, res) => {
-  // if (!res.locals.current_user) {
-  //   res.redirect(401, "/login");
-  //   return;
-  // }
   const shortUrl = req.params.shortUrl;
   delete urlDatabase[shortUrl];
   res.redirect('/urls');
 });
 
-app.post("/urls/:shortURL", (req, res) => {
+app.post("/urls/:shortURL", loggedInCheck, checkURLOwnership, (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
   urlDatabase[shortURL].longURL = longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
-
-
-app.get("/urls/:shortURL", loggedInCheck, (req, res) => {
-  // TODO: figure out if we have an error
-
-  // if (urlDatabase.user_id !== res.locals.current_user) {
-  //   res.status(403);
-  //   res.send("Tiny URL does not match user");
-  // }
-  if (urlDatabase.shortURL === undefined) {
-    res.status(404);
-    res.send("Tiny URL does not exist");
-  }
-// TODO figure out how to check if user matches shortURL
-
+app.get("/urls/:shortURL", loggedInCheck, checkURLOwnership, (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL
@@ -203,19 +195,17 @@ app.get("/urls/:shortURL", loggedInCheck, (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  for (let id in urlDatabase) {
-    if (id === req.params.shortURL) {
-      let longURL = urlDatabase[id].longURL;
-      res.redirect(longURL);
-    }
+  let urlEntry = urlDatabase[req.params.shortURL]
+  if (urlEntry) {
+    res.redirect(urlEntry.longURL);
+    return;
+  } else {
+    res.status(404);
+    res.send("URL does not exist");
+    return;
   }
-  res.redirect(404, "/urls");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
